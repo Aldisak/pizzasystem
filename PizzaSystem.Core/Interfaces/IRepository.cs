613657b309dce63b4a95d2f721271c1a0eb09554
Aldisak <47724529+Aldisak@users.noreply.github.com>
2023-06-10 11:36:13 +0200

    Created persistance layer (#2)
    Co-authored-by: alesmatejka <ales.matejka@valatron.com>

--- PizzaSystem.Core/Interfaces/IRepository.cs
+++ PizzaSystem.Core/Interfaces/IRepository.cs
@@ -1,5 +1,3 @@
-using PizzaSystem.Core.Models;
-
 namespace PizzaSystem.Core.Interfaces;
 
 public interface IRepository<T>
@@ -7,7 +5,7 @@ public interface IRepository<T>
     ID<T> Add(T entity);
     ID<T> Update(T entity);
     ID<T> Delete(T entity);
-    T Get(ID<T> id);
+    T? Get(ID<T> id);
     IEnumerable<T> GetAll();
     IEnumerable<T> GetAllById(ID<T> id);
 }
