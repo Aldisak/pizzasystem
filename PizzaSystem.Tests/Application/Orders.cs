f2a4f25a1f12ccbac431e78ef7a4c4b03315754d
alesmatejka <ales.matejka@valatron.com>
2023-06-14 18:29:04 +0200

    fixed tests

--- PizzaSystem.Tests/Application/Orders.cs
+++ PizzaSystem.Tests/Application/Orders.cs
@@ -23,53 +23,72 @@ namespace Tests.Application
         public async Task Get_ValidId_ReturnsOkResult()
         {
             // Arrange
-            var orderId = 1;
-            var order   = GenerateOrder();
-            _orderServiceMock.Setup(service => service.Get(order.Id)).ReturnsAsync(order);
+            var expectedOrder = GenerateOrder();
+            _orderServiceMock.Setup(service => service.Get(expectedOrder.Id))
+                             .ReturnsAsync(expectedOrder);
 
             // Act
-            var result = await _orderController.Get(id: orderId) as OkObjectResult;
+            var result = await _orderController.Get(expectedOrder.Id);
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.That(result!.StatusCode, Is.EqualTo(200));
-            Assert.That(result.Value, Is.EqualTo(order));
-            _orderServiceMock.Verify(service => service.Get(order.Id), Times.Once);
+            Assert.NotNull(result);
+            Assert.That(result, Is.EqualTo(expectedOrder));
         }
 
         [Test]
-        public async Task Update_ValidOrder_ReturnsOkResult()
+        public async Task Add_ValidOrder_ReturnsCreatedAtActionResult()
         {
             // Arrange
-            var order = GenerateOrder();
-            _orderServiceMock.Setup(service => service.Update(order)).ReturnsAsync(order.Id);
+            var       orderToAdd = GenerateOrder();
+            var       addedOrder = GenerateAnotherOrder();
+        
+            _orderServiceMock.Setup(service => service.Add(orderToAdd))
+                             .ReturnsAsync(addedOrder);
 
             // Act
-            var result = await _orderController.Update(order) as OkObjectResult;
+            var result = await _orderController.Add(orderToAdd.Id, orderToAdd);
+
+            // Assert
+            Assert.IsInstanceOf<CreatedAtActionResult>(result);
+            var createdAtActionResult = result;
+            Assert.Multiple(() =>
+            {
+                Assert.That(createdAtActionResult.ActionName, Is.EqualTo(nameof(OrderController.Get)));
+                Assert.That(createdAtActionResult.RouteValues!["id"], Is.EqualTo(addedOrder.Id));
+                Assert.That(createdAtActionResult.Value, Is.EqualTo(addedOrder));
+            });
+        }
+
+        [Test]
+        public async Task Update_ValidOrder_ReturnsUpdatedOrder()
+        {
+            // Arrange
+            var       updatedOrder = GenerateOrder();
+            _orderServiceMock.Setup(service => service.Update(updatedOrder))
+                             .ReturnsAsync(updatedOrder);
+
+            // Act
+            var result = await _orderController.Update(updatedOrder.Id, updatedOrder);
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.That(result!.StatusCode, Is.EqualTo(200));
-            Assert.That(result.Value, Is.EqualTo(order.Id));
-            _orderServiceMock.Verify(service => service.Update(order), Times.Once);
+            Assert.That(result, Is.EqualTo(updatedOrder));
         }
         
         [Test]
-        public async Task Delete_ValidId_ReturnsOkResult()
+        public async Task Delete_ValidId_ReturnsDeletedOrder()
         {
             // Arrange
-            var orderId = 1;
-            var order   = GenerateOrder();
-            _orderServiceMock.Setup(service => service.Delete(order.Id)).ReturnsAsync(order.Id);
+            var       deletedOrder = GenerateOrder();
+            _orderServiceMock.Setup(service => service.Delete(deletedOrder.Id))
+                             .ReturnsAsync(deletedOrder);
 
             // Act
-            var result = await _orderController.Delete(id: orderId) as OkObjectResult;
+            var result = await _orderController.Delete(deletedOrder.Id);
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.That(result!.StatusCode, Is.EqualTo(200));
-            Assert.That(result.Value, Is.EqualTo(order.Id));
-            _orderServiceMock.Verify(service => service.Delete(order.Id), Times.Once);
+            Assert.That(result, Is.EqualTo(deletedOrder));
         }
         
         [Test]
@@ -80,12 +99,11 @@ namespace Tests.Application
             _orderServiceMock.Setup(service => service.GetAll()).ReturnsAsync(orders);
 
             // Act
-            var result = await _orderController.GetAll() as OkObjectResult;
+            var result = await _orderController.GetAll();
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.That(result!.StatusCode, Is.EqualTo(200));
-            Assert.That(result.Value, Is.EqualTo(orders));
+            Assert.That(result, Is.EqualTo(orders));
             _orderServiceMock.Verify(service => service.GetAll(), Times.Once);
         }
 
@@ -95,24 +113,70 @@ namespace Tests.Application
             {
                 Id         = 1,
                 OrderItems = new List<OrderItem>(),
-                CustomerContact = new CustomerContact
+                CustomerContact = CustomerContact(),
+                Total = 1500,
+                OrderDetails = OrderDetails()
+            };
+
+            CustomerContact CustomerContact()
+            {
+                return new CustomerContact
                 {
                     CustomerName = new CustomerName {FirstName = "Aleš", LastName = "Matějka"},
                     PhoneNumber  = new Phone {CountryCode      = "+420", Number   = 722007626},
                     Address = new Address
                     {
-                        Street = "Vršovická", HouseNumber = "7/27", City = "Praha", State = "Česká republika", ZipCode = "10100"
+                        Street  = "Vršovická", HouseNumber = "7/27", City = "Praha", State = "Česká republika",
+                        ZipCode = "10100"
                     }
-                },
-                Total = 1500,
-                OrderDetails = new OrderDetails
+                };
+            }
+            OrderDetails OrderDetails()
+            {
+                return new OrderDetails
                 {
-                    Status       = 0, 
-                    DeliveryType = 0, 
+                    Status       = OrderStatus.Pending,
+                    DeliveryType = DeliveryType.Delivery,
                     CreatedAt    = new DateTime(2021, 06, 10, 19, 23, 47),
                     UpdatedAt    = new DateTime(2021, 07, 10, 19, 23, 47)
-                }
+                };
+            }
+        }
+
+        private static Order GenerateAnotherOrder()
+        {
+            return new Order
+            {
+                Id         = 2,
+                OrderItems = new List<OrderItem>(),
+                CustomerContact = CustomerContact(),
+                Total = 1300,
+                OrderDetails = OrderDetails()
             };
+
+            CustomerContact CustomerContact()
+            {
+                return new CustomerContact
+                {
+                    CustomerName = new CustomerName {FirstName = "Milan", LastName = "Hruška"},
+                    PhoneNumber  = new Phone {CountryCode      = "+420", Number   = 606125169},
+                    Address = new Address
+                    {
+                        Street  = "Gruzínská", HouseNumber = "17", City = "Praha", State = "Česká republika",
+                        ZipCode = "10100"
+                    }
+                };
+            }
+            OrderDetails OrderDetails()
+            {
+                return new OrderDetails
+                {
+                    Status       = OrderStatus.Pending, 
+                    DeliveryType = DeliveryType.TakeAway, 
+                    CreatedAt    = new DateTime(2021, 04, 10, 19, 23, 47),
+                    UpdatedAt    = new DateTime(2021, 04, 10, 20, 23, 47)
+                };
+            }
         }
     }
 }
