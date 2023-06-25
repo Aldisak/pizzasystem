769680769719da0b48b98e642b785cb0ddd227ed
alesmatejka <ales.matejka@valatron.com>
2023-06-25 10:47:50 +0200

    Odstraneni dynamickeho nacitani, protoze ho nepotrebuju

--- PizzaSystem.Core/Extensions.cs
+++ PizzaSystem.Core/Extensions.cs
@@ -1,9 +1,5 @@
-using System.Reflection;
-using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using PizzaSystem.Core.Commands;
 using PizzaSystem.Core.Interfaces;
-using PizzaSystem.Core.Models;
 using PizzaSystem.Core.Services;
 
 namespace PizzaSystem.Core;
@@ -13,22 +9,7 @@ public static class Extensions
     public static IServiceCollection AddCoreServices(this IServiceCollection services)
     {
         services.AddScoped(typeof(IService<>), typeof(Service<>));
-        
-        // Register handlers dynamically from the assembly PizzaSystem.Core according to the IRequestHandler<,> interface
-        var assembly = Assembly.GetExecutingAssembly();
-        foreach (var type in assembly.GetTypes())
-        {
-            if (!type.IsClass || type.IsAbstract) continue;
-            var interfaces = type.GetInterfaces();
-            foreach (var @interface in interfaces)
-            {
-                if (@interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
-                {
-                    services.AddScoped(@interface, type);
-                }
-            }
-        }
-        
+
         return services;
     }
 }
